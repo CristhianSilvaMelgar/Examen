23fa49580597d0476825b6322cc9415217d38870
GATM <german.gt285@gmail.com>
2019-05-21 23:03:29 -0400

    Examen2

--- app/src/main/java/com/univalle/examen/Main2Activity.java
+++ app/src/main/java/com/univalle/examen/Main2Activity.java
@@ -1,15 +1,20 @@
 package com.univalle.examen;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
-    Button calcular;
-    EditText Resultado,unit;
+    Button calcular,atras;
+    EditText unit;
+    TextView Resultado;
     Double Unidad,Res;
     String val,codigo;
 
@@ -17,13 +22,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Bundle bundle = getIntent().getExtras();
+
         Resultado=findViewById(R.id.textResultado);
         calcular=findViewById(R.id.Calcular);
         unit=findViewById(R.id.Unidad);
-        Unidad=Double.parseDouble(unit.getText().toString());
-        //codigo=bundle.getString("codigo");
-        /*switch (codigo){
+        atras=findViewById(R.id.Atras);
+
+        codigo=getIntent().getStringExtra("codigo");
+        //Resultado.setText(codigo);
+
+        switch (codigo){
+                //Temperatura
             case "1":
                 this.setTitle(R.string.btnTemperatura1);
                 break;
@@ -36,36 +45,144 @@ public class Main2Activity extends AppCompatActivity {
             case "4":
                 this.setTitle(R.string.btnTemperatura4);
                 break;
+                //Area
+            case "5":
+                this.setTitle(R.string.btnArea1);
+                break;
+            case "6":
+                this.setTitle(R.string.btnArea2);
+                break;
+            case "7":
+                this.setTitle(R.string.btnArea3);
+                break;
+            case "8":
+                this.setTitle(R.string.btnArea4);
+                break;
+                //Distancia
+            case "9":
+                this.setTitle(R.string.btnDistancia1);
+                break;
+            case "10":
+                this.setTitle(R.string.btnDistancia2);
+                break;
+            case "11":
+                this.setTitle(R.string.btnDistancia3);
+                break;
+            case "12":
+                this.setTitle(R.string.btnDistancia4);
+                break;
             default:
                 this.setTitle(R.string.app_name);
                 break;
         }
+        cargarPreferencias();
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                guardarPreferencias();
+                try{
+                    String aux=unit.getText().toString();
+                    Unidad=Double.parseDouble(aux);
+                }catch (NumberFormatException nfe){}
+
                 switch (codigo){
                     case "1":
-                        Res=Unidad*2-Unidad/5;
+                        Res=(Unidad*1.8)+32;
                         val=String.valueOf(Res);
                         Resultado.setText(val+" Fº");
                         break;
                     case "2":
-                        Res=Unidad*2-Unidad/5;
+                        Res=(Unidad-32)/1.8;
                         val=String.valueOf(Res);
-                        Resultado.setText(val+" Fº");
+                        Resultado.setText(val+" Cº");
                         break;
                     case "3":
-                        Res=Unidad*2-Unidad/5;
+                        Res=273.15+Unidad;
                         val=String.valueOf(Res);
                         Resultado.setText(val+" Fº");
                         break;
                     case "4":
-                        Res=Unidad*2-Unidad/5;
+                        Res=Unidad-273.15;
                         val=String.valueOf(Res);
                         Resultado.setText(val+" Fº");
                         break;
+                    case "5":
+                        Res=Unidad*10000;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" m^2");
+                        break;
+                    case "6":
+                        Res=Unidad*0.0001;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" Hect");
+                        break;
+                    case "7":
+                        Res=Unidad*0.000001;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" Km^2");
+                        break;
+                    case "8":
+                        Res=Unidad*1000000;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" m^2");
+                        break;
+                    //Distancia
+                    case "9":
+                        Res=Unidad*100;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" cm");
+                        break;
+                    case "10":
+                        Res=Unidad/100;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" m");
+                        break;
+                    case "11":
+                        Res=Unidad*0.39370;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" inch");
+                        break;
+                    case "12":
+                        Res=Unidad*2.54;
+                        val=String.valueOf(Res);
+                        Resultado.setText(val+" cm");
+                        break;
                 }
             }
-        });*/
+        });
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+    private void guardarPreferencias() {
+        //IMPORTAMOS LA LIBRERIA SHARED PREFERENCES
+        SharedPreferences prefs =
+                getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+
+        String UltimoNumero=unit.getText().toString();
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("Nombre",UltimoNumero);
+
+       unit.setText(UltimoNumero);
+
+        editor.commit();
+
+    }
+
+    private  void cargarPreferencias(){
+
+        SharedPreferences prefs =
+                getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+        String UltimoNumero = prefs.getString("Nombre","No Existe");
+
+        unit.setText(UltimoNumero);
     }
 }
--- app/src/main/java/com/univalle/examen/MainActivity.java
+++ app/src/main/java/com/univalle/examen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.univalle.examen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,25 +11,149 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnCelciusFahren,btnFahrenCelcius,btnFahrenKelvin,btnCelciusKelvin;
+    Button btnHectMet2,btnMet2Hect,btnMet2Kil2,btnKil2Met2;
+    Button btnMetCen,btnCenMet,btnCenInch,btnInchCen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btnCelciusFahren=findViewById(R.id.btnCenFah);
         btnFahrenCelcius=findViewById(R.id.btnFahCen);
         btnFahrenKelvin=findViewById(R.id.btnFahKel);
         btnCelciusKelvin=findViewById(R.id.btnCenKel);
 
+        btnHectMet2=findViewById(R.id.btnHectMet);
+        btnMet2Hect=findViewById(R.id.btnMetHect);
+        btnMet2Kil2=findViewById(R.id.btnmetKil);
+        btnKil2Met2=findViewById(R.id.btnKilMet);
 
+        btnMetCen=findViewById(R.id.btnMetCen);
+        btnCenMet=findViewById(R.id.btnCenMet);
+        btnCenInch=findViewById(R.id.btnCenInch);
+        btnInchCen=findViewById(R.id.btnInchCen);
+
+
+        //Temp
+        btnCelciusFahren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","1");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnFahrenCelcius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","2");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnFahrenKelvin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","3");
+                startActivity(intent);
+                finish();
+            }
+        });
 
         btnCelciusKelvin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), Main2Activity.class);
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
                 intent.putExtra("codigo","4");
-                startActivityForResult(intent, 0);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Area
+
+        btnHectMet2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","5");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnMet2Hect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","6");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnMet2Kil2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","7");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnKil2Met2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","8");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Distancia
+
+        btnMetCen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","9");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnCenMet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","10");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnCenInch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","11");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnInchCen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("codigo","12");
+                startActivity(intent);
+                finish();
             }
         });
     }
+
 
 }
